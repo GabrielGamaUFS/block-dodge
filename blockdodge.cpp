1cd3453c6965b0bb618b2941d10710b970aa0a6c
Gabriel Gama <145946554+GabrielGamaUFS@users.noreply.github.com>
2025-03-18 15:24:02 -0300

    Implementação do jogador
    Verifica se o botão foi pressionado e muda o jogador de linha.

--- blockdodge.cpp
+++ blockdodge.cpp
@@ -1,15 +1,25 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 7, 6, 5, 4); // Ordem da pinagem do display LCD
+LiquidCrystal lcd(12, 11, 7, 6, 5, 4); // ordem da pinagem do display LCD
 
+const int botao = 2;
 const int larguraTela = 16;
 
-char blocoCima[larguraTela];
-char blocoBaixo[larguraTela];
+char blocoCima[larguraTela + 1];
+char blocoBaixo[larguraTela + 1];
+
+bool botaoApertado = false;
+bool jogadorEmCima = false;
 
 int tempoObstaculo = 0;
 
+void botaoPressionado() {
+    botaoApertado = true;
+}
+
 void setup() {
+    // Função de interrupção que verifica em tempo real se o botão foi pressionado. Evita a verificação dentro do loop, na qual causa conflito com a movimentação dos blocos
+	attachInterrupt(digitalPinToInterrupt(botao), botaoPressionado, FALLING);    
     lcd.begin(16, 2);
 
     // Inicia a tela sem blocos
@@ -18,9 +28,18 @@ void setup() {
 }
 
 void loop() {
+    if (botaoApertado) {
+        jogadorEmCima = !jogadorEmCima;
+        botaoApertado = false;
+    }
+
+    // Move os obstáculos/blocos para a esquerda
+    memmove(blocoCima, blocoCima + 1, larguraTela - 1);
+    memmove(blocoBaixo, blocoBaixo + 1, larguraTela - 1);
+
     // Adiciona um novo obstáculo/bloco aleatório ao array, se tempo == 0
     if (tempoObstaculo == 0) {
-    	if (random(2) == 0) {
+        if (random(2) == 0) {
             blocoCima[larguraTela - 1] = '#';
             blocoBaixo[larguraTela - 1] = ' ';
         }
@@ -38,13 +57,13 @@ void loop() {
     }
 
     // Desenha no display
-    lcd.setCursor(0, 0);
+    lcd.clear();
+    lcd.setCursor(0, jogadorEmCima ? 0 : 1);
+    lcd.print('X'); // formato do jogador
+    lcd.setCursor(1, 0);
     lcd.print(blocoCima);
-    lcd.setCursor(0, 1);
+    lcd.setCursor(1, 1);
     lcd.print(blocoBaixo);
-    // Move os obstáculos/blocos para a esquerda
-    memmove(blocoCima, blocoCima + 1, larguraTela - 1);
-    memmove(blocoBaixo, blocoBaixo + 1, larguraTela - 1);
 
     // Controla a velocidade do jogo
     delay(200); // Fazer função para diminuir conforme o tempo/pontuação passe
